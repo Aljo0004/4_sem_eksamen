28e37217b66d564d372ee74e72da04114e1f7fa2
axel <axsj0001@stud.kea.dk>
2026-05-11 15:08:52 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/components/Location.jsx
+++ src/app/components/Location.jsx
@@ -1,5 +1,16 @@
 const Location = () => {
-  return <div></div>;
+  return (
+    <div className="ml-10">
+      <div className="card-body">
+        <h2 className="--fontsize-h4-mobil">Kom ned i galleriet</h2>
+        <div>
+          <p>Vores adresse: St. Kongensgade 3, 1264 København K</p>
+          <p>Åbningstider: Man-fre: 08:00 - 16:00 Lør-søn: 12:00 - 15:00</p>
+        </div>
+        <img src="/images/Location_img.jpg" alt="kort" />
+      </div>
+    </div>
+  );
 };
 
 export default Location;
