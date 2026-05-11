da8c66792fa6df9999b53e116c2e218b9741cc47
axel <axsj0001@stud.kea.dk>
2026-05-11 15:29:10 +0200

    Refactor Location component for improved styling consistency

--- src/app/components/Location.jsx
+++ src/app/components/Location.jsx
@@ -2,8 +2,8 @@ const Location = () => {
   return (
     <div className="ml-5">
       <div className="card-body">
-        <h4 className="--fontsize-h4-mobil">Kom ned i galleriet</h4>
-        <div>
+        <h4 className="--fontsize-h4-mobil font-lighter">Kom ned i galleriet</h4>
+        <div className="grid grid-cols-2 md:grid-cols-2 gap-2 font-bold">
           <p>Vores adresse: St. Kongensgade 3, 1264 København K</p>
           <p>Åbningstider: Man-fre: 08:00 - 16:00 Lør-søn: 12:00 - 15:00</p>
         </div>
