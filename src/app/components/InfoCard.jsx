abafec51e9e5945c17801c117bf5d02d2f67d326
axel <axsj0001@stud.kea.dk>
2026-05-16 10:24:57 +0200

    Refactor layout structure and typography for InfoCard and LearnMore components

--- src/app/components/InfoCard.jsx
+++ src/app/components/InfoCard.jsx
@@ -3,9 +3,11 @@ const InfoCard = () => {
     <div className="bg-(--primary-blue) mb-10 md:flex md:items-stretch mt-10">
       <div className="flex-1 p-5 md:p-6">
         <div className="bg-(--primary-blue) h-1 w-26 mb-3 mt-4 md:mt-3"></div>
-        <p className="text-sm mb-2">Om KÆNTT</p>
-        <h3 className="text-lg md:text-sm mb-2 lg:!text-3xl">Hvem er Kæntt?</h3>
-        <h5 className="text-sm mb-3 leading-snug lg:!text-lg lg:pb-10">Lær historien bag galleriet, vores mission og de kunstnere vi samarbejder med..</h5>
+        <p className="t-card mb-2">Om KÆNTT</p>
+
+        <h3 className="t-h3 mb-2">Hvem er Kæntt?</h3>
+
+        <p className="t-p mb-3">Lær historien bag galleriet, vores mission og de kunstnere vi samarbejder med.</p>
         <a href="#" className="underline text-primary text-(--secondary-blue)">
           Læs mere →
         </a>
