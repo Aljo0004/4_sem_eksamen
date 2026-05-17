8a86bf3f779ad641ca92d0ff27a550c76a80926c
Aljo0004 <aljo0004@stud.kea.dk>
2026-05-17 13:11:52 +0200

    Refactor artist and product components for improved layout and functionality

--- src/app/components/ArtistInfo.jsx
+++ src/app/components/ArtistInfo.jsx
@@ -3,21 +3,19 @@ import Link from "next/link";
 
 const ArtistInfo = ({ artists }) => {
   return (
-    <div className="group relative w-full overflow-hidden mb-12 bg-white">
-      <Link href={`/artists/${artists.id}`} className="block w-full ">
-        <article className="w-full bg-white ml-16">
-          <div className="aspect-3/2 w-full max-w-sm">
-            <img src={artists?.image_url} alt={artists?.name || "Artwork"} className="h-full mb-4 w-full object-cover transition-transform duration-300 ease-out group-hover:scale-95" />
-          </div>
-          <div className="">
+    <div className="relative w-full overflow-hidden mb-12 bg-white">
+      <article className="">
+        <Link href={`/artists/${artists.id}`} className="block ">
+          <img src={artists?.image_url} alt={artists?.name || "Artwork"} className="w-full h-full aspect-3/2 object-cover transition-transform duration-300 ease-out hover:scale-95" />
+          <div className="place-content-end">
             <div className="bg-(--primary-blue) max-w-sm h-2 w-28 mt-4 mb-4"></div>
             <div className="flex flex-col justify-between">
               <p className="text-xl capitalize font-bold leading-tight">{artists?.name}</p>
-              <p className="font-semibold">{artists?.description?.substring(0, 100)}...</p>
+              <p className="font-semibold max-w-md">{artists?.description?.substring(0, 100)}...</p>
             </div>
           </div>
-        </article>
-      </Link>
+        </Link>
+      </article>
     </div>
   );
 };
--- src/app/components/Productcard.jsx
+++ src/app/components/Productcard.jsx
@@ -3,27 +3,30 @@
 import Link from "next/link";
 import AddToCart from "./AddToCart";
 
-const Productcard = ({ artwork }) => {
+const Productcard = ({ artwork, pinDetailsToBottom = false }) => {
   return (
     <div className="group relative w-full max-w-xs overflow-hidden mb-12 bg-white">
-      <Link href={`/productlist/${artwork.id}`} className="block w-full max-w-xs">
-        <article className="w-full max-w-xs overflow-hidden bg-white ">
-          <div className="relative aspect-squared w-full">
+      <article className={`w-full max-w-xs overflow-hidden bg-white ${pinDetailsToBottom ? "flex h-full min-h-140 flex-col" : ""}`}>
+
+          <div className="relative w-full">
             <div className="absolute z-10 right-5 bottom-5 opacity-0 transition-opacity duration-200 ease-out group-hover:opacity-100 group-focus-within:opacity-100">
               <AddToCart id={artwork?.id} title={artwork?.name} price={artwork?.price} thumbnail={artwork?.thumbnail_url} />
             </div>
-            <img src={artwork?.thumbnail_url} alt={artwork?.name || "Artwork"} className="h-full mb-4 w-full object-cover transition-transform duration-300 ease-out group-hover:scale-95" />
+            <Link href={`/productlist/${artwork.id}`} className="block w-full max-w-xs">
+          <div className="relative w-full"></div>
+            <img src={artwork?.thumbnail_url} alt={artwork?.name || "Artwork"} className="h-full w-full aspect-squared object-cover transition-transform duration-300 ease-out group-hover:scale-95" />
+         </Link>
           </div>
-          <div className="">
-            <div className="bg-(--primary-blue) h-2 w-28 mt-4 mb-4 group-hover:w-full duration-300 ease-inOut"></div>
-            <div className="flex justify-between">
-              <p className="text-xl capitalize font-bold leading-tight">{artwork?.name}</p>
-              <p className="font-semibold">{artwork?.price} kr.</p>
-            </div>
-            <p className="mt-2 line-clamp-3 text-sm">{artwork?.description?.substring(0, 75)}...</p>
+
+        <div className={pinDetailsToBottom ? "mt-auto" : ""}>
+          <div className="bg-(--primary-blue) h-2 w-28 mt-4 mb-4 group-hover:w-full duration-300 ease-in-out"></div>
+          <div className="flex justify-between">
+            <p className="text-xl capitalize font-bold leading-tight">{artwork?.name}</p>
+            <p className="font-semibold">{artwork?.price} kr.</p>
           </div>
-        </article>
-      </Link>
+          <p className="mt-2 line-clamp-3 text-sm">{artwork?.description?.substring(0, 75)}...</p>
+        </div>
+      </article>
     </div>
   );
 };
