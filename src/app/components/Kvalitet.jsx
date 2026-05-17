b68d5b94c6e359bb654fd33b96831f9be856a10d
axel <axsj0001@stud.kea.dk>
2026-05-17 16:18:56 +0200

    Refactor Kvalitet and LearnMoreSection components for improved layout and readability

--- src/app/components/Kvalitet.jsx
+++ src/app/components/Kvalitet.jsx
@@ -1,17 +1,23 @@
 const Kvalitet = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-6 items-stretch mb-10">
-      <div className="w-full md:w-1/2 order-2 md:order-1">
-        <div className="bg-(--primary-blue) h-1 w-26 mb-2"></div>
-        <h3>Kvalitetssikring i hver detalje</h3>
-        <h5>
-          Vi arbejder tæt sammen med vores talentfulde kunstnere og printer deres værker på vores eksklusive Hahnemühle Matte Art Paper 210 g/m². Hvert enkelt print bliver omhyggeligt håndteret. Vi kan garantere, at du modtager et produkt af højeste kvalitet, samtidig med at vi undgår behovet for at opretholde store lagerbeholdninger. Vi mener, at kvalitet over kvantitet er vejen frem, og vi undgår masseproduktion for at bevare hvert kunstværks unikke karakter. Vi printer alle ordrer efter behov på bestilling. Dette sikrer, at vi kun producerer det, der reelt bestilles, hvilket reducerer spild og giver en mere miljøvenlig tilgang til produktionen. Unikke kunstværker kræver omhu, når de skabes. Vi printer, snitter, indrammer, montere hver dag med ønsket om du får lige det værk, du ønsker
-          dig.
-        </h5>
-      </div>
+    <div className=" pt-10">
+      <div className="flex flex-col lg:flex-row lg:justify-between items-start">
+        {/* Text column: stays above image on mobile, left on desktop */}
+        <div className="order-2 w-full lg:w-480px lg:order-1 pb-10">
+          <div className="">
+            <div className="bg-(--primary-blue) h-2 w-24 mb-2 mt-5 lg:mt-0"></div>
+            <h2 className="t-h2 pb-5">Kvalitetssikring i hver detalje</h2>
+            <p className="t-p lg:pr-45">
+              Vi arbejder tæt sammen med vores talentfulde kunstnere og printer deres værker på vores eksklusive Hahnemühle Matte Art Paper 210 g/m². Hvert enkelt print bliver omhyggeligt håndteret. Vi kan garantere, at du modtager et produkt af højeste kvalitet, samtidig med at vi undgår behovet for at opretholde store lagerbeholdninger. <br></br>
+              <br></br> Vi mener, at kvalitet over kvantitet er vejen frem, og vi undgår masseproduktion for at bevare hvert kunstværks unikke karakter. Vi printer alle ordrer efter behov på bestilling. Dette sikrer, at vi kun producerer det, der reelt bestilles, hvilket reducerer spild og giver en mere miljøvenlig tilgang til produktionen. <br></br>
+              <br></br>Unikke kunstværker kræver omhu, når de skabes. Vi printer, snitter, indrammer, montere hver dag med ønsket om du får lige det værk, du ønsker dig.
+            </p>
+          </div>
+        </div>
 
-      <div className="w-full md:w-1/2 order-1 md:order-2">
-        <img src="/images/Learn-More-kvalitet_img.jpg" alt="Hvem er" className="w-full h-full object-cover" />
+        <div className="w-full lg:w-1/2 lg:order-2 lg:flex">
+          <img src="/images/Learn-More-kvalitet_img.jpg" alt="Learn more" className="" />
+        </div>
       </div>
     </div>
   );
