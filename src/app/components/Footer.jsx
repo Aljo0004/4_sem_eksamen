3e3fea3c856b87b09a98d25fe1720e48096ec2bd
axel <axsj0001@stud.kea.dk>
2026-05-17 11:49:55 +0200

    Refactor footer layout and update input fields for better accessibility

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,15 +6,15 @@ const Footer = () => {
     <footer className="footer sm:footer-horizontal bg-base-300 text-base-content p-10 bg-(--primary-blue)">
       {/* Nav grid */}
 
-      <div className="w-full grid grid-cols-1 lg:grid-cols-5 gap-6 lg:gap-8 items-start">
-        <nav className="w-full">
+      <div className="w-full grid grid-cols-1 lg:flex gap-6 items-start">
+        <nav className="w-full lg:w-300">
           <h4 className="t-h4 footer-title">Adresse</h4>
           <p className="t-p">
             St. Kongensgade 3,<br></br>1264 København K
           </p>
         </nav>
 
-        <nav className="w-full">
+        <nav className="w-400">
           <h4 className="t-h4 footer-title">Åbningstider</h4>
           <p className="t-p">
             Man-fre: 08:00 - 16:00 <br></br>Lørdag & søndag: 12:00 - 15:00
@@ -58,17 +58,20 @@ const Footer = () => {
           </ul>
         </nav>
 
-        <nav className="w-full">
+        <nav className="w-full lg:w-600">
           <h4 className="t-h4 footer-title">Nyhedsbrev</h4>
           <p className="t-p text-sm mt-3 leading-relaxed">
             Nyhedsbrevet er til dig, der gerne vil holde sig opdateret om Dansk kvindesamfunds events og nyheder.
             <br />
+            <br></br>
             Du kan til enhver tid framelde dig igen!
           </p>
 
           <div className="mt-3 flex flex-col gap-2">
-            <input type="text" placeholder="Fuldt navn" className="input input-bordered w-full t-p" />
-            <input type="email" placeholder="Email" className="input input-bordered w-full t-p" />
+            <p className="t-p">Fuldt navn</p>
+            <input type="text" placeholder="Udfyld her" className=" bg-white input input-bordered w-full t-p pt-1 pb-1" />
+            <p className="t-p">E-Mail</p>
+            <input type="email" placeholder="Udfyld her" className="bg-white input input-bordered w-full t-p pt-1 pb-1" />
           </div>
         </nav>
       </div>
@@ -76,14 +79,14 @@ const Footer = () => {
       <div className="w-full mt-6 flex flex-col items-center">
         <img src="/Images/logo.svg" alt="kaentt logo" className="h-20 object-contain mb-3" />
         <nav aria-label="Sociale medier" className="flex gap-6 items-center text-2xl mt-2">
-          <a href="#" aria-label="TikTok" className="hover:opacity-80">
-            <FaTiktok />
+          <a href="#" aria-label="Facebook" className="hover:opacity-80">
+            <FaFacebook />
           </a>
           <a href="#" aria-label="Instagram" className="hover:opacity-80">
             <FaInstagram />
           </a>
-          <a href="#" aria-label="Facebook" className="hover:opacity-80">
-            <FaFacebook />
+          <a href="#" aria-label="TikTok" className="hover:opacity-80">
+            <FaTiktok />
           </a>
         </nav>
       </div>
